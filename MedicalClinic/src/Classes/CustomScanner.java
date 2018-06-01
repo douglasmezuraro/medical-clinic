2abd47218e3650d56d827e4360a4663bee3c41a1
Victor Glauber Lopes Silva <victor_glauber@hotmail.com>
2018-05-31 22:16:37 -0300

    Validações do CustomScanner

--- MedicalClinic/src/Classes/CustomScanner.java
+++ MedicalClinic/src/Classes/CustomScanner.java
@@ -7,53 +7,88 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class CustomScanner {
-    
+
     private Scanner input;
-    
+
     public CustomScanner() {
         input = new Scanner(System.in);
     }
-    
+
     public String readString(String question) {
-        System.out.println(question);
-        return input.nextLine();
+        try {
+            System.out.println(question);
+            return input.nextLine();
+        } catch (Exception e) {
+            return null;
+        }
     }
-    
+
     public byte readByte(String question) {
-        return Byte.parseByte(readString(question));
+        try {
+            return Byte.parseByte(readString(question));
+        } catch (Exception e) {
+            {
+                return 0;
+            }
+        }
     }
-    
+
     public short readShort(String question) {
-        return Short.parseShort(readString(question));
-    }   
-    
+        try {
+            return Short.parseShort(readString(question));
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
     public int readInt(String question) {
-        return Integer.parseInt(readString(question));
+        try {
+            return Integer.parseInt(readString(question));
+        }catch (Exception e){
+            return 0;
+        }
     }
-    
+
     public long readLong(String question) {
-        return Long.parseLong(readString(question));
+        try{
+            return Long.parseLong(readString(question));
+        } catch (Exception e) {
+            return 0;
+        }
+        
     }
-    
+
     public float readFloat(String question) {
-        return Float.parseFloat(readString(question));
+        try {
+            return Float.parseFloat(readString(question));
+        }catch (Exception e){
+            return 0;
+        }
     }
-    
+
     public double readDouble(String question) {
-        return Double.parseDouble(readString(question));
+        try {
+            return Double.parseDouble(readString(question));
+        }catch (Exception e){
+            return 0;
+        }
     }
-    
+
     public Date readDate(String question) {
         try {
             SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
             return format.parse(readString(question));
-        } catch(ParseException e) {
+        } catch (ParseException e) {
             return null;
         }
     }
-    
+
     public Time readTime(String question) {
-        return Time.valueOf(readString(question));
+        try{
+            return Time.valueOf(readString(question));
+        } catch(Exception e){
+            return null;
+        }
     }
-    
+
 }
